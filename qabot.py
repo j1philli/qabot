bba31ba677b347d3c2d6cc046e9fca299211d06a
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-08 13:28:41 -0600

    make first MVP

--- qabot.py
+++ qabot.py
@@ -1,4 +1,9 @@
 from maubot import Plugin
+from maubot.handlers import event
+from mautrix.types import EventType, StateEvent
 
 class qabot(Plugin):
-  pass
+  @event.on(EventType.ROOM_MESSAGE)
+  async def handle_message(self, evt: StateEvent) -> None:
+    self.log.info(f"{evt.room_id} received a message")
+    await self.client.send_text(evt.room_id, "Received message")
