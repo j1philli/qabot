69b12636b2d4a7589f00176779470c736e1ae0a2
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-10 03:26:03 -0600

    implemenet response for replies

--- qabot.py
+++ qabot.py
@@ -8,4 +8,9 @@ class QaBot(Plugin):
     self.log.info(f"{evt.room_id} received a message")
 
     if evt.sender != self.client.mxid or evt.content['org.matrix.msc2716.historical'] != True:
-      await self.client.send_text(evt.room_id, "Received message")
+      await self.client.send_text(evt.room_id, "Received normal message")
+
+  @event.on(EventType.ROOM_MESSAGE)
+  async def handle_reply(self, evt: MessageEvent) -> None:
+    if evt.sender != self.client.mxid or evt.content['org.matrix.msc2716.historical'] != True:
+      await self.client.send_text(evt.room_id, "reply received", relates_to=evt.event_id)
