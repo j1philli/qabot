9da79bfb4ea955f022570dd1d7f7885ceadf539f
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-10 16:25:28 -0600

    refactor into one room message event and implement proper reply filter

--- qabot.py
+++ qabot.py
@@ -6,11 +6,16 @@ class QaBot(Plugin):
   @event.on(EventType.ROOM_MESSAGE)
   async def handle_message(self, evt: MessageEvent) -> None:
     self.log.info(f"{evt.room_id} received a message")
-
-    if evt.sender != self.client.mxid or evt.content['org.matrix.msc2716.historical'] != True:
+    # default = evt.sender != self.client.mxid and 'org.matrix.msc2716.historical' not in evt.content
+    # messageTypes = {
+    #   'reply' : default and 'm.relates_to' in evt.content
+    # }
+    if 'm.relates_to' in evt.content:
+      self.log.info("true")
+    else:
+      self.log.info("false")
+    self.log.info(evt.content)
+    if evt.sender != self.client.mxid and 'org.matrix.msc2716.historical' not in evt.content and evt.content._relates_to != None:
+      await self.client.send_text(evt.room_id, "Received reply message")
+    elif evt.sender != self.client.mxid and 'org.matrix.msc2716.historical' not in evt.content:
       await self.client.send_text(evt.room_id, "Received normal message")
-
-  @event.on(EventType.ROOM_MESSAGE)
-  async def handle_reply(self, evt: MessageEvent) -> None:
-    if evt.sender != self.client.mxid or evt.content['org.matrix.msc2716.historical'] != True:
-      await self.client.send_text(evt.room_id, "reply received", relates_to=evt.event_id)
