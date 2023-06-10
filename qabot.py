26bbb29eb4342b619ff063f6c2fc5a15bef63bbe
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-10 02:46:00 -0600

    add filtering for backfilled messages

--- qabot.py
+++ qabot.py
@@ -1,9 +1,11 @@
 from maubot import Plugin
 from maubot.handlers import event
-from mautrix.types import EventType, StateEvent
+from mautrix.types import EventType, MessageEvent
 
 class QaBot(Plugin):
   @event.on(EventType.ROOM_MESSAGE)
-  async def handle_message(self, evt: StateEvent) -> None:
+  async def handle_message(self, evt: MessageEvent) -> None:
     self.log.info(f"{evt.room_id} received a message")
-    await self.client.send_text(evt.room_id, "Received message")
+
+    if evt.sender != self.client.mxid or evt.content['org.matrix.msc2716.historical'] != True:
+      await self.client.send_text(evt.room_id, "Received message")
