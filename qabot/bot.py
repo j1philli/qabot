501cf44151bfc21b169be78c08c7474e90e60ef3
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-11 17:22:31 -0600

    format

--- qabot/bot.py
+++ qabot/bot.py
@@ -2,7 +2,6 @@ from typing import Type
 from maubot import Plugin
 from maubot.handlers import event
 from mautrix.types import EventType, MessageEvent
-from websockets.server import serve
 import asyncio
 from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 
@@ -11,15 +10,13 @@ class Config(BaseProxyConfig):
         helper.copy("server-ip")
         helper.copy("server-port")
 
-async def main(self):
-  async with serve(echo, self.config["server-host"], self.config["server-ip"]):
-    await asyncio.Future()  # run forever
-
 class QaBot(Plugin):
   async def start(self) -> None:
     self.config.load_and_update()
-  
-  m
+
+  @classmethod
+  def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
 
   @event.on(EventType.ROOM_MESSAGE)
   async def handle_message(self, evt: MessageEvent) -> None:
@@ -34,8 +31,4 @@ class QaBot(Plugin):
   async def handle_reactions(self, evt: MessageEvent) -> None:
     if evt.sender != self.client.mxid:
       self.log.info(f"{evt.room_id} received a reaction I care about")
-      await self.client.send_text(evt.room_id, "Received reaction")
-
-  @classmethod
-  def get_config_class(cls) -> Type[BaseProxyConfig]:
-        return Config
+      await self.client.send_text(evt.room_id, "Received reaction")  
