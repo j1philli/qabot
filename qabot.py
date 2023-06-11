37b7f282dc138dfd2cf0ba54e6807324a36b52ee
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-11 13:49:59 -0600

    fix type errors with import add config items to classs

--- qabot.py
+++ qabot.py
@@ -1,3 +1,4 @@
+from typing import Type
 from maubot import Plugin
 from maubot.handlers import event
 from mautrix.types import EventType, MessageEvent
@@ -7,20 +8,16 @@ from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
 
 class Config(BaseProxyConfig):
     def do_update(self, helper: ConfigUpdateHelper) -> None:
-        helper.copy("reddit_base")
+        helper.copy("server-ip")
+        helper.copy("server-port")
 
-async def main():
-    async with serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+async def main(self):
+  async with serve(echo, self.config["server-host"], self.config["server-ip"]):
+    await asyncio.Future()  # run forever
 
 class QaBot(Plugin):
   async def start(self) -> None:
-        await super().start()
-        self.config.load_and_update()
-
-  @classmethod
-  def get_config_class(cls) -> Type[BaseProxyConfig]:
-        return Config
+    self.config.load_and_update()
 
   @event.on(EventType.ROOM_MESSAGE)
   async def handle_message(self, evt: MessageEvent) -> None:
@@ -36,3 +33,7 @@ class QaBot(Plugin):
     if evt.sender != self.client.mxid:
       self.log.info(f"{evt.room_id} received a reaction I care about")
       await self.client.send_text(evt.room_id, "Received reaction")
+
+  @classmethod
+  def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
