ab08542118081c6f3fe74eabc71304f492a1439a
Josh Phillips <drag0n2ooth230@gmail.com>
2023-06-11 13:30:52 -0600

    add config capabilities

--- qabot.py
+++ qabot.py
@@ -3,12 +3,25 @@ from maubot.handlers import event
 from mautrix.types import EventType, MessageEvent
 from websockets.server import serve
 import asyncio
+from mautrix.util.config import BaseProxyConfig, ConfigUpdateHelper
+
+class Config(BaseProxyConfig):
+    def do_update(self, helper: ConfigUpdateHelper) -> None:
+        helper.copy("reddit_base")
 
 async def main():
     async with serve(echo, "localhost", 8765):
         await asyncio.Future()  # run forever
 
 class QaBot(Plugin):
+  async def start(self) -> None:
+        await super().start()
+        self.config.load_and_update()
+
+  @classmethod
+  def get_config_class(cls) -> Type[BaseProxyConfig]:
+        return Config
+
   @event.on(EventType.ROOM_MESSAGE)
   async def handle_message(self, evt: MessageEvent) -> None:
     if evt.sender != self.client.mxid and 'org.matrix.msc2716.historical' not in evt.content and evt.content._relates_to != None:
